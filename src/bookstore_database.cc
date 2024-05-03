6bb75208d35edf4730548f35114903deaf25debc
SaharaSurfer <st119029@student.spbu.ru>
2024-05-03 12:30:25 +0300

    Migrate to using book_view in database queries
    Updated get_summaries and get_book_details methods to use the new
    book_view in the database, which simplifies queries and improves performance.
    
    Signed-off-by: SaharaSurfer <st119029@student.spbu.ru>

--- src/bookstore_database.cc
+++ src/bookstore_database.cc
@@ -5,7 +5,8 @@
 
 std::vector<std::string> BookstoreDatabase::get_summaries(int num_books,
                                                           int start_index) {
-  std::string query = "SELECT * FROM summary_view LIMIT $1 OFFSET $2";
+  std::string query = "SELECT title, author_name, price "
+                      "FROM book_view LIMIT $1 OFFSET $2";
 
   try {
     pqxx::result res = transaction_.exec_params(query, num_books, start_index);
@@ -33,14 +34,8 @@ std::vector<std::string> BookstoreDatabase::get_summaries(int num_books,
 }
 
 std::string BookstoreDatabase::get_book_details(const std::string& isbn) {
-  std::string query = "SELECT b.isbn, b.title, g.genre_name, "
-                      "       a.author_name, p.publisher_name, "
-                      "       b.publication_date, b.price "
-                      "FROM book b "
-                      "JOIN genre g ON b.genre_id = g.genre_id "
-                      "JOIN author a ON b.author_id = a.author_id "
-                      "JOIN publisher p ON b.publisher_id = p.publisher_id "
-                      "WHERE b.isbn = $1";
+  std::string query = "SELECT * FROM book_view "
+                      "WHERE isbn = $1";
   
   try {
     pqxx::result res = transaction_.exec_params(query, isbn);
