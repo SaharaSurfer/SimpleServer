794f22aa4740e0e920c031dd5106d915d1081bf2
SaharaSurfer <st119029@student.spbu.ru>
2024-05-08 14:08:16 +0300

    Modify main.cc
    The main file has been modified to match the changes in the Server class.
    main.cc creates a database instance and starts the server.
    
    Signed-off-by: SaharaSurfer <st119029@student.spbu.ru>

--- src/server/main.cc
+++ src/server/main.cc
@@ -1,29 +1,15 @@
 #include <iostream>
 #include <string>
 
-#include "header/bookstore_database.h"
+#include "header/server.h"
 
 int main() {
-  std::string connection_data = std::getenv("DATABASE_URL");
-
-  if (connection_data.empty()) {
-    std::cerr << "DATABASE_URL environment variable not set" << std::endl;
-    return 1;
-  }
-
   try {
-    BookstoreDatabase based(connection_data);
+    std::string connection_data = std::getenv("DATABASE_URL");
+    BookstoreDatabase db(connection_data);
 
-    std::string divider(80, '-');
+    Server server(8080, db);
 
-    std::string test_1 = based.get_book_details("978-1-40-885565-2");
-    std::cout << test_1 << divider << "\n";
-
-    std::vector<std::string> test_2 = based.get_summaries(10, 1);
-    for (const std::string& row : test_2) {
-      std::cout << row << divider << "\n";
-    }
-    
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return 1;
