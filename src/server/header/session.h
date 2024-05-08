f0f72a1b3b12483b317c47d7dca6d932335b71c4
SaharaSurfer <st119029@student.spbu.ru>
2024-05-08 13:40:26 +0300

    Add a function to read the request.
    Added a function that reads a user request into the input buffer
    and logs an error if one occurs.
    
    Signed-off-by: SaharaSurfer <st119029@student.spbu.ru>

--- src/server/header/session.h
+++ src/server/header/session.h
@@ -1,28 +1,23 @@
 #ifndef SIMPLESERVER_HEADER_SESSION_H_
 #define SIMPLESERVER_HEADER_SESSION_H_
 
-#include <string>
-
+#include <utility>
 #include <boost/asio.hpp>
 
 #include "bookstore_database.h"
 
-using namespace boost::asio;
-
 class Session {
  private:
-  ip::tcp::socket socket_;
-  streambuf data_;
-  BookstoreDatabase& database_;
+  boost::asio::ip::tcp::socket socket_;
+  boost::asio::streambuf input_buffer_;
+  BookstoreDatabase& db_;
 
-  void receive_request();
-  void handle_request();
+  void read_request();
 
  public:
-  Session(ip::tcp::socket socket, BookstoreDatabase& database)  
-    : socket_(std::move(socket)), database_(database) {}
+  Session(boost::asio::ip::tcp::socket socket, BookstoreDatabase& db)
+    : socket_(std::move(socket)), db_(db) {};
 
-  void start() { receive_request(); };
 };
 
 #endif // SIMPLESERVER_HEADER_SESSION_H_
--- src/server/session.cc
+++ src/server/session.cc
@@ -1,5 +1,8 @@
 #include "header/session.h"
 
+#include <iostream>
+#include <string>
+#include <utility>
 #include <boost/asio.hpp>
 
 #include "header/bookstore_database.h"
@@ -13,6 +16,23 @@ void Session::receive_request() {
   }
 }
 
-void Session::handle_request() {
+void Session::read_request() {
+  try {
+    boost::system::error_code ec;
+
+    // Clear input buffer
+    input_buffer_.consume(input_buffer_.size());
+
+    read_until(socket_, input_buffer_, "\r\n", ec);
+
+    if (ec) {
+      throw boost::system::system_error(ec);
+    }
+
+  } catch (const std::exception& e) {
+    std::cerr << "Error in Session::read_request(): " << e.what() << std::endl;
+  }
+}
+
 
 }
