1dc271e3ce0f0edd3faa7482f4a2f3d8f4de35db
SaharaSurfer <st119029@student.spbu.ru>
2024-05-18 09:48:01 +0300

    Make Server asynchronous
    The Server class now uses boost asio's built-in thread_pool.
    Instead of accept, async_accept is used.
    
    Signed-off-by: SaharaSurfer <st119029@student.spbu.ru>

--- src/server/header/server.h
+++ src/server/header/server.h
@@ -1,12 +1,11 @@
 #ifndef SIMPLESERVER_HEADER_SERVER_H_
 #define SIMPLESERVER_HEADER_SERVER_H_
 
+#include <atomic>
 #include <vector>
-#include <thread>
 #include <utility>
 #include <boost/asio.hpp>
 
-#include "request_queue.h"
 #include "bookstore_database.h"
 
 class Server {
@@ -15,18 +14,17 @@ class Server {
   boost::asio::ip::tcp::acceptor acceptor_;
   BookstoreDatabase& db_;
 
-  RequestQueue request_queue_;
-  std::vector<std::thread> worker_threads_;
-  bool stop_ = false;
+  std::atomic<bool> stop_{false};
+  boost::asio::thread_pool pool_;
 
   void begin_accept_connections();
   void set_up_acceptor(unsigned short port);
-  void worker_thread();
 
  public:
   Server(unsigned short port, BookstoreDatabase& db);
   ~Server();
   void run();
+  void stop();
 };
 
 #endif // SIMPLESERVER_HEADER_SERVER_H_
--- src/server/server.cc
+++ src/server/server.cc
@@ -10,53 +10,41 @@
 #include "header/session.h"
 
 Server::Server(unsigned short port, BookstoreDatabase& db)
-  : acceptor_(io_context_), db_(db) {
-  
+    : acceptor_(io_context_), db_(db), pool_(std::thread::hardware_concurrency()) {
   set_up_acceptor(port);
 }
 
 Server::~Server() {
+  stop();
+}
+
+void Server::run() {
+  begin_accept_connections();
+  
+  boost::asio::post(pool_, [this] { io_context_.run(); });
+  pool_.join();
+}
+
+void Server::stop() {
   stop_ = true;
-  request_queue_.stop();
   acceptor_.close();
   io_context_.stop();
 }
 
-void Server::run() {
-  unsigned int num_threads = std::max(2u, std::thread::hardware_concurrency());
-  worker_threads_.reserve(num_threads);
-  for (unsigned int i = 0; i < num_threads; ++i) {
-    worker_threads_.emplace_back(&Server::worker_thread, this);
-  }
-
-  begin_accept_connections();
-
-  for (auto& thread : worker_threads_) {
-    thread.join();
-  }
-}
-
 void Server::begin_accept_connections() {
-  while (!stop_) {
-    try {
-      boost::asio::ip::tcp::socket socket(io_context_);
-      acceptor_.accept(socket);
-      std::cout << "Connection established" << std::endl;
-
-      auto session = std::make_shared<Session>(std::move(socket), db_, request_queue_);
-      session->start();
-
-    } catch (const boost::system::system_error& e) {
-      if (e.code() == boost::asio::error::bad_descriptor) {
-        // This error occurs when the socket is closed
-        std::cerr << "Socket closed. Continuing to accept connections." << std::endl;
-        continue;
-      } else {
-        // Other errors
-        std::cerr << "Error accepting connection: " << e.what() << std::endl;
-      }
+  auto new_session = std::make_shared<Session>(io_context_, db_);
+  acceptor_.async_accept(new_session->socket(),
+                         [this, new_session](const boost::system::error_code& error) {
+    if (!error) {
+      new_session->start();
+    } else {
+      std::cerr << "Error accepting connection: " << error.message() << std::endl;
     }
-  }
+
+    if (!stop_) {
+      begin_accept_connections();
+    }
+  });
 }
 
 void Server::set_up_acceptor(unsigned short port) {
@@ -68,15 +56,4 @@ void Server::set_up_acceptor(unsigned short port) {
   acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
   acceptor_.bind(endpoint);
   acceptor_.listen();
-}
-
-void Server::worker_thread() {
-  while (true) {
-    Request request(nullptr, "", {});
-    if (!request_queue_.pop(request)) {
-      break;
-    }
-
-    request.session->process_request(request.type, request.params);
-  }
 }
